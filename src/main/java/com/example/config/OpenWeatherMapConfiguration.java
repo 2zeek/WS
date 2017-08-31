021057cb545179b27e3a0f8eb19e240c45bb0fa8
petrovnv <S3HkaHaE>
2017-08-31 17:51:33 +0300

    fix

--- src/main/java/com/example/config/OpenWeatherMapConfiguration.java
+++ src/main/java/com/example/config/OpenWeatherMapConfiguration.java
@@ -2,13 +2,17 @@ package com.example.config;
 
 import com.example.config.properties.OpenWeatherMapProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(OpenWeatherMapProperties.class)
 public class OpenWeatherMapConfiguration {
 
     @Autowired
     OpenWeatherMapProperties openWeatherMapProperties;
+
+    @Bean
+    public OpenWeatherMapProperties getOpenWeatherMapProperties() {
+        return openWeatherMapProperties;
+    }
 }
--- src/main/java/com/example/config/properties/OpenWeatherMapProperties.java
+++ src/main/java/com/example/config/properties/OpenWeatherMapProperties.java
@@ -1,25 +1,25 @@
 package com.example.config.properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
-@PropertySource(value = "file:${config.directory}/openweathermap.properties")
+@Component
+@PropertySource("file:${config.directory}/openweathermap.properties")
 @ConfigurationProperties(prefix = "weather")
 @Configuration
 public class OpenWeatherMapProperties {
 
+    @Value("${weather.server}")
     private String server;
+    @Value("${weather.params}")
     private String params;
 
-    public OpenWeatherMapProperties openWeatherMapProperties() {
-        OpenWeatherMapProperties newProp = new OpenWeatherMapProperties();
-        newProp.server = System.getProperty("weather.server");
-        newProp.params = System.getProperty("weather.params");
-        return newProp;
-    }
-
     public String getServer() {
+        System.out.println((System.getProperty("weather.server")));
+        System.out.println(System.getProperty("weather.params"));
         return server;
     }
 
